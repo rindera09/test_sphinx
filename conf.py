7841a9ace6c4ad620d1c9598a888749746116601
rindera <rindera09@gmail.com>
2016-12-20 12:30:51 +0800

    second commit

--- conf.py
+++ conf.py
@@ -20,6 +20,11 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# add 2016/12/20
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {'.md': CommonMarkParser}
+
 
 # -- General configuration ------------------------------------------------
 
@@ -38,8 +43,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
@@ -82,7 +87,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
